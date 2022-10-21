5bddec3836de6b4f1a50cee77842977b2cee2fb5
kanilkumarreddy <116055966+kanilkumarreddy@users.noreply.github.com>
2022-10-21 17:48:19 +0530

    Update settings.py

--- Simple_login/settings.py
+++ Simple_login/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     }
 }
 
-
+CSRF_TRUSTED_ORIGINS = ['https://your-domain.com']
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
